1ac2cc38ee706ef5ecc54a97ef0c9bfdf77f12bd
milan_weerasinghe <milanmmw123@gmail.com>
2024-11-25 08:42:51 +0530

    changed the beahvior in profile picture

--- src/components/sections/main/AboutSection.jsx
+++ src/components/sections/main/AboutSection.jsx
@@ -5,27 +5,25 @@ const About = () => {
         <div>
 
             <p className='mb-4'>
-                I'm a 4th-year Computer Science undergraduate at the University of Sri Jayewardenepura, Sri Lanka, deeply passionate
+                I'm a 4th-year Computer Science undergraduate at the <span className='text-slate-200 font-medium'>University of Sri Jayewardenepura,</span> Sri Lanka, deeply passionate
                 about building impactful software and leveraging technology to solve real-world problems. My project
-                experience spans various applications, from developing a simple <a className="highlight-text" 
-                href="https://github.com/MilanWeerasinghe/MusicApp" target='_blank' rel='noreferrer noopener'>artist management system</a> to
-                microservice-based solutions for hotel booking management.
+                experience spans various applications, from developing simple structured programming to
+                microservice-based architecture.
             </p>
 
             <p className='mb-4'>
                 In my work, I have utilized languages and
                 technologies such as Java, JavaScript, and SQL, I am also familiar with front-end frameworks like React
-                and backend tools like Spring Boot. I bring a solid understanding of software development methodologies,
-                including agile practices and the principles of SOLID design, to build maintainable and efficient code. Eager
-                to expand my expertise further, I am especially interested in getting hands-on experience with <span className='highlight-text'>AI/ML and
-                full-stack development</span>. Through coding, I aspire to make meaningful contributions to innovative projects,
+                and backend frameworks like Spring Boot. I bring a solid understanding of software development methodologies,
+                including agile practices and the principles of SOLID design, to build maintainable and efficient code. I am especially interested in getting hands-on experience with <span className='text-slate-200 font-medium'>AI/ML and
+                full-stack development.</span> Through coding, I aspire to make meaningful contributions to innovative projects,
                 continuously improve my skills, and ultimately drive positive change in technology and society.
             </p>
 
             <p className='mb-4'>
                 Thank you for visiting my portfolio website! I'm thrilled to have you here. Feel free to explore and learn
                 more about me, my projects, and my journey as a software engineering enthusiast. Whether you're here to connect,
-                collaborate, or simply browse, I hope you find something that inspires you. Let's create something amazing together!
+                collaborate, or simply browse, I hope you find something that inspires you.<br /> <span className='font-medium'>Let's create something amazing together!</span>
             </p>
         </div>
     )
